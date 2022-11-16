305e9580d1683a9bacf31eaeb842c038e0ab2d3d
kwame <kwame.agyenimboateng@petraonline.com>
2022-11-16 19:02:56 +0000

    extras

---
+++ Uber/Constants/Color.swift
@@ -0,0 +1,14 @@
+//
+//  Color.swift
+//  Uber
+//
+//  Created by Kwame Agyenim - Boateng on 16/11/2022.
+//
+
+import UIKit
+
+
+struct Color {
+    static let black = UIColor.black.withAlphaComponent(0.9)
+    static let blue = UIColor(red: 0.15, green: 0.43, blue: 0.95, alpha: 1.00)
+}
---
+++ Uber/Extensions/UIView+Ext.swift
@@ -0,0 +1,27 @@
+//
+//  UIView+Ext.swift
+//  Uber
+//
+//  Created by Kwame Agyenim - Boateng on 16/11/2022.
+//
+
+import UIKit
+
+extension UIView {
+    
+    func pin(to superview: UIView){
+        translatesAutoresizingMaskIntoConstraints = false
+        topAnchor.constraint(equalTo: superview.safeAreaLayoutGuide.topAnchor).isActive = true
+        leadingAnchor.constraint(equalTo: superview.leadingAnchor).isActive = true
+        trailingAnchor.constraint(equalTo: superview.trailingAnchor).isActive = true
+        bottomAnchor.constraint(equalTo: superview.safeAreaLayoutGuide.bottomAnchor).isActive = true
+    }
+    
+    func pinToEdges(to superview: UIView){
+        translatesAutoresizingMaskIntoConstraints = false
+        topAnchor.constraint(equalTo: superview.topAnchor).isActive = true
+        leadingAnchor.constraint(equalTo: superview.leadingAnchor).isActive = true
+        trailingAnchor.constraint(equalTo: superview.trailingAnchor).isActive = true
+        bottomAnchor.constraint(equalTo: superview.bottomAnchor).isActive = true
+    }
+}
