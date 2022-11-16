754f29db2e9dca94df5c0fe32ffb301c79af076c
kwame <kwame.agyenimboateng@petraonline.com>
2022-11-16 10:06:15 +0000

    added fonts

---
+++ Uber/Constants/Font.swift
@@ -0,0 +1,16 @@
+//
+//  Font.swift
+//  Uber
+//
+//  Created by Kwame Agyenim - Boateng on 16/11/2022.
+//
+
+import Foundation
+
+
+enum Font: String {
+    case bold = "UberMove-Bold"
+    case medium = "UberMove-Medium"
+    case light = "UberMove-Light"
+    case regular = "UberMove-Regular"
+}
