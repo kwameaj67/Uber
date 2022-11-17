adafc450b374b190973c76dce8479345c007a182
kwame <kwame.agyenimboateng@petraonline.com>
2022-11-17 19:17:38 +0000

    anchor layout shortcut

--- Uber/Extensions/UIView+Ext.swift
+++ Uber/Extensions/UIView+Ext.swift
@@ -24,4 +24,44 @@ extension UIView {
         trailingAnchor.constraint(equalTo: superview.trailingAnchor).isActive = true
         bottomAnchor.constraint(equalTo: superview.bottomAnchor).isActive = true
     }
+    
+    func anchor(top: NSLayoutYAxisAnchor? = nil,
+                left: NSLayoutXAxisAnchor? = nil,
+                right: NSLayoutXAxisAnchor? = nil,
+                bottom: NSLayoutYAxisAnchor? = nil,
+                paddingTop: CGFloat = 0,
+                paddingLeft: CGFloat = 0,
+                paddingRight: CGFloat = 0,
+                paddingBottom: CGFloat = 0,
+                width: CGFloat? = nil,
+                height: CGFloat? = nil
+    ){
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        if let top = top {
+            topAnchor.constraint(equalTo: top, constant: paddingTop).isActive = true
+        }
+        if let left = left {
+            leadingAnchor.constraint(equalTo: left, constant: paddingLeft).isActive = true
+        }
+        if let right = right {
+            trailingAnchor.constraint(equalTo: right, constant: -paddingRight).isActive = true
+        }
+        if let bottom = bottom {
+            bottomAnchor.constraint(equalTo: bottom, constant: -paddingBottom).isActive = true
+        }
+        if let width = width {
+            widthAnchor.constraint(equalToConstant: width).isActive = true
+        }
+        if let height = height {
+            heightAnchor.constraint(equalToConstant: height).isActive = true
+        }
+    }
+    
+    func centerX(in view: UIView){
+        centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    }
+    func centerY(in view: UIView){
+        centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
 }
