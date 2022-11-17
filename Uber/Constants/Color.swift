8a00fa6386907eb32a480059e614947dea230044
kwame <kwame.agyenimboateng@petraonline.com>
2022-11-17 19:17:19 +0000

    new colours

--- Uber/Constants/Color.swift
+++ Uber/Constants/Color.swift
@@ -11,4 +11,6 @@ import UIKit
 struct Color {
     static let black = UIColor.black.withAlphaComponent(0.9)
     static let blue = UIColor(red: 0.15, green: 0.43, blue: 0.95, alpha: 1.00)
+    static let text_grey = UIColor(red: 0.46, green: 0.46, blue: 0.46, alpha: 1.00)
+    static let grey_bg = UIColor(red: 0.93, green: 0.93, blue: 0.93, alpha: 1.00)
 }
