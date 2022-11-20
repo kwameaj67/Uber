4a429f4ec596daeec99a75c407566d3bebe4c2bb
kwame <kwame.agyenimboateng@petraonline.com>
2022-11-20 23:27:46 +0000

    added tabBarItem bouncy animation

--- Uber/Controllers/TabBar/TabBarVC.swift
+++ Uber/Controllers/TabBar/TabBarVC.swift
@@ -82,7 +82,28 @@ class TabBarVC: UITabBarController {
             }
         }
     }
-
     
+    // bouncy tab bar icon animation
+    override func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
+        let orderedTabBarItemViews: [UIView] = {
+            let interactionViews = tabBar.subviews.filter({ $0 is UIControl })
+            return interactionViews.sorted(by: { $0.frame.minX < $1.frame.minX })
+        }()
+        guard
+            let index = tabBar.items?.firstIndex(of: item),
+            let subview = orderedTabBarItemViews[index].subviews.first
+        else {
+            return
+        }
+        performSpringAnimation(for: subview)
+    }
+    func performSpringAnimation(for view: UIView) {
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseInOut, animations: {
+                view.transform = CGAffineTransform(scaleX: 1.08, y: 1.08)
+                UIView.animate(withDuration: 0.5, delay: 0.2, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseInOut, animations: {
+                    view.transform = CGAffineTransform(scaleX: 1, y: 1)
+                }, completion: nil)
+            }, completion: nil)
+    }
 
 }
