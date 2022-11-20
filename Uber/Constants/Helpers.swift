1a203d79a964dbba010edcfba601d9118259f0c4
kwame <kwame.agyenimboateng@petraonline.com>
2022-11-20 23:30:17 +0000

    helpers

---
+++ Uber/Constants/Helpers.swift
@@ -0,0 +1,14 @@
+//
+//  Helpers.swift
+//  Uber
+//
+//  Created by Kwame Agyenim - Boateng on 20/11/2022.
+//
+
+import Foundation
+import UIKit
+
+
+func delay(duration: Double, completion: @escaping () -> Void ){
+    DispatchQueue.main.asyncAfter(deadline: .now() + duration, execute: completion)
+}
