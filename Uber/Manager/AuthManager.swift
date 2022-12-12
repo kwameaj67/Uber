aa302ba7ef35c12c0be158d6f4eca62b5ecd9039
kwameagyenim-boateng <kwame.agyenim-boateng@petraonline.com>
2022-12-12 18:30:16 +0000

    created auth manager & google firebase file

---
+++ Uber/Manager/AuthManager.swift
@@ -0,0 +1,23 @@
+//
+//  AuthManager.swift
+//  Uber
+//
+//  Created by Kwame Agyenim - Boateng on 12/12/2022.
+//
+
+import Foundation
+
+
+struct AuthManager {
+    
+    static let shared = AuthManager()
+    
+    private init() {}
+    
+    
+    // create user
+    
+    // login user
+    
+    // log out user
+}
