6e3bf5e2364d50398c83ef1137c0ca75c507a615
kwameagyenim-boateng <kwame.agyenim-boateng@petraonline.com>
2022-12-20 09:31:57 +0000

    etc

--- Uber/Extensions/UILabel+Ext.swift
+++ Uber/Extensions/UILabel+Ext.swift
@@ -10,12 +10,18 @@ import UIKit
 extension UILabel {
   
     func letterSpacing(value: CGFloat) {
-    if let textString = self.text {
-      let attributedString = NSMutableAttributedString(string: textString)
-        attributedString.addAttribute(NSAttributedString.Key.kern, value: value, range: NSRange(location: 0, length: attributedString.length - 1))
-      attributedText = attributedString
+        if let textString = self.text {
+          let attributedString = NSMutableAttributedString(string: textString)
+            attributedString.addAttribute(NSAttributedString.Key.kern, value: value, range: NSRange(location: 0, length: attributedString.length - 1))
+          attributedText = attributedString
+        }
     }
-  }
+    func addCharacterSpacing(kernValue: Double = 1.15) {
+        guard let text = text, !text.isEmpty else { return }
+        let string = NSMutableAttributedString(string: text)
+        string.addAttribute(NSAttributedString.Key.kern, value: kernValue, range: NSRange(location: 0, length: string.length - 1))
+        attributedText = string
+    }
     
     func setLineHeight(lineHeight: CGFloat) {
         let paragraphStyle = NSMutableParagraphStyle()
