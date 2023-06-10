87ee735dadb506fa07e23de322a1cee956a14fd1
kwame_boateng <kwameagyenimboateng64@gmail.com>
2023-06-10 16:45:29 +0000

    * animate subclass of button

--- Uber/Views/Components/UberButton.swift
+++ Uber/Views/Components/UberButton.swift
@@ -9,17 +9,53 @@ import UIKit
 
 class UberButton: UIButton {
 
+    private var animator: UIViewPropertyAnimator?
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
         //MARK:  properties -
-        backgroundColor = .black
-        setTitleColor(.white, for: .normal)
-        titleLabel?.font = UIFont(name: Font.medium.rawValue, size: 18)
+        translatesAutoresizingMaskIntoConstraints = false
+        // setup button targets
+        addTarget(self, action: #selector(handleTouchDown), for: .touchDown)
+        addTarget(self, action: #selector(handleTouchUpInside), for: .touchUpInside)
+        addTarget(self, action: #selector(handleTouchUpOutside), for: .touchUpOutside)
+        
+        addTarget(self, action: #selector(handleHoverIn), for: .touchDragEnter)
+        addTarget(self, action: #selector(handleHoverOut), for: .touchDragExit)
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    @objc func handleTouchDown(){
+        animateButton(scaleX: 0.97, scaleY: 0.97)
+    }
+    
+    @objc func handleTouchUpInside(){
+        animateButton(scaleX: 1.0, scaleY: 1.0)
+    }
+    
+    @objc func handleTouchUpOutside(){
+        animateButton(scaleX: 1.0, scaleY: 1.0)
+    }
+    
+    @objc func handleHoverIn(){
+        animateButton(scaleX: 1.02, scaleY: 1.02)
+    }
+    
+    @objc func handleHoverOut(){
+        animateButton(scaleX: 1.0, scaleY: 1.0)
+    }
+    
+    private func animateButton(scaleX: CGFloat, scaleY: CGFloat){
+        animator?.stopAnimation(true)
+        
+        animator = UIViewPropertyAnimator(duration: 0.5, dampingRatio: 0.8,animations: {
+            self.transform = CGAffineTransform(scaleX: scaleX, y: scaleY)
+        })
+        
+        animator?.startAnimation()
+    }
 }
