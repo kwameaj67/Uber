1a15f5d8cd4f6dd2efea2c254b812721b5bd6d9d
Kwame Agyenim - Boateng <kwameagyenimboateng64@gmail.com>
2023-07-26 15:26:52 +0000

    separated enums from mock models

--- Uber/Models/MockData.swift
+++ Uber/Models/MockData.swift
@@ -7,16 +7,7 @@
 
 import CoreLocation
 
-enum AccountType: String {
-    case rider = "rider"
-    case driver = "driver"
-}
 
-
-enum RecentLocationType{
-    case work
-    case random
-}
 struct RecentLocation {
     var name: String
     var location: String
@@ -43,10 +34,6 @@ struct UberFeatureOption {
     ]
 }
 
-enum TripStatus{
-    case cancelled
-    case booked
-}
 struct Trip{
     var location: String
     var date: String
@@ -80,14 +67,7 @@ class UserLocation {
     }
 }
 
-enum ActionOptionIconType{
-    case messages
-    case settings
-    case business
-    case earning
-    case legal
-    case refer
-}
+
 struct AccountOption{
     var iconType: ActionOptionIconType
     var name: String
@@ -103,11 +83,7 @@ struct AccountOption{
     ]
 }
 
-enum AccountActionType{
-    case help
-    case wallet
-    case trips
-}
+
 struct AccountActionOption {
     var name: String
     var icon: String?
@@ -122,33 +98,7 @@ struct AccountActionOption {
 
 
 
-// MARK: Settings data
-enum DescriptionData: String {
-    case home = "Unnamed Road"
-    case work = "Petra Trust Company Limited"
-    case shortcut =  "Managed saved location"
-    case privacy = "Manage the data you share with us"
-    case security = "Control your account security with 2-setup verification"
-    case appearance = "Use device settings"
-    case trusted_contacts = "Share your trip status with Family and friends in a single tap"
-    case verify_trip = "Use a PIN to make sure you get in the right car"
-    case ride_check = "Manage your RideCheck notifications"
-    case setup_family = "Pay for your loved ones and get trip notifications"
-    case signout = "Signout"
-}
-enum OptionType {
-    case home
-    case work
-    case shortcut
-    case privacy
-    case security
-    case appearance
-    case trusted_contacts
-    case verify_trip
-    case ride_check
-    case setup_family
-    case signout
-}
+
 struct SectionOption{
     var name: String
     var icon: String?
@@ -191,3 +141,16 @@ struct Section {
 
 }
 
+// MARK: EditAccountData -
+struct EditAccountData{
+    var dataType: EditAccountDataType
+    
+    
+    static let data: [EditAccountData] = [
+        EditAccountData(dataType: .firstname),
+        EditAccountData(dataType: .lastname),
+        EditAccountData(dataType: .phone),
+        EditAccountData(dataType: .email),
+        EditAccountData(dataType: .phone),
+    ]
+}
